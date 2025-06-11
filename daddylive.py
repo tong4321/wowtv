7d87a6cfc52b45d28679238365127e81b44c1635
mehmetey03 <mehmeteyigun6@gmail.com>
2025-06-12 02:33:43 +0300

    Update daddylive.py

--- daddylive.py
+++ daddylive.py
@@ -281,7 +281,7 @@ def format_channel(channel):
 
 def main():
    
-    url = "https://kerimmkirac-daddyliveevents.hf.space/playlist/channels"
+    url = "https://mehmetey03-dadyytv.hf.space/playlist/channels"
     content = get_playlist_from_url(url)
     
     if not content:
